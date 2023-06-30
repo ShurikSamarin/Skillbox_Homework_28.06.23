f7a702ece09381c4ecc24287902865f0672b01fe
Alexander <sttp2002@gmail.com>
2023-06-30 21:40:22 +0300

    first

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+class Program{
+    static void Main(string[]args){
+    Console.Clear();
+    Console.WriteLine("Input sentence");
+    string [] words = SplitText(Console.ReadLine()!);
+    string [] reverseWords = Reverse(words);
+    Console.WriteLine("Words array:"); 
+    PrintArray (reverseWords);
+    static void PrintArray (string [] array){
+        foreach (string word in array){
+        Console.WriteLine(word);
+        }
+    }
+    static string [] SplitText(string sentence){
+        string [] arr = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        return arr;
+    }
+    static string [] Reverse(string [] arr){
+        string [] reverseArr = new string [arr.Length];
+        for (int i = 0; i<arr.Length; i++){
+            reverseArr[i] = arr[arr.Length-1-i];
+        }
+        return reverseArr;
+
+    } 
+    }
+}
